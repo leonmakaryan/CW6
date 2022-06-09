af8d4be8e3c2468b8525f704f82f54d0b86d6b96
Leon Makaryan <70325164+leonmakaryan@users.noreply.github.com>
2022-06-09 19:03:53 +0300

    Update main.cpp
    added mersenne twister PRNG + little bit of code optimisation
    added writing to a file
    working on changing list of edges to adjacency list...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <fstream>
+#include "mersenne_twister.h"
 
-std::vector<int> path{1,2,3,4}; // paths we are looking for
+std::vector<int> path{1,1,2,1,1}; // paths we are looking for
+int random_seed = 1;
 
 void finderRec(std::vector<std::tuple<int,int,int>>& edges, int& p, int k, int& TotalNumber){
     for (int i = 0; i < edges.size(); ++i){
@@ -37,8 +40,9 @@ std::vector<std::tuple<int,int,int>> generateGraph(int& n, std::vector<int>& W){
     for (int i = 1; i <= n; ++i){
         j = i + 1;
         while (j <= n){
-            random = rand() % len;
-            sel_elem = W[random];
+            CRandomMersenne randgen_three(random_seed);
+            random_seed++;
+            sel_elem = W[randgen_three.IRandom(0, len)];
             edges.push_back({i,j,sel_elem});
             j++;
         }
@@ -51,14 +55,26 @@ int main(){
     int n; // number of vertices
     std::cout << "Input the number of vertices n: ";
     std::cin >> n;
-    std::vector<int> W{1,2,3,4,5}; // set of labels
+    int m; // number of tests in Monte-Carlo
+    std::cout << "Input the number of tests m: ";
+    std::cin >> m;
+    std::vector<int> W{1,2,3,4,5,6}; // set of labels
     //std::cout << "Input the desired set of labels W ";
     //std::vector<int> path{1,2,3}; // paths we are looking for
-    std::vector<std::tuple<int,int,int>> edges = generateGraph(n, W);
     //for (auto& x: edges)
       //cj  std::cout << std::get<2>(x) << " ";
     
     std::cout << "\n";
-    std::cout << "Total number of paths = " <<  pathFinder(path, edges) << "\n";
+    //std::cout << "Total number of paths = " <<  pathFinder(path, edges) << "\n";
+    
+    std::ofstream out;// поток для записи
+    out.open("MonteCarlo.txt"); // окрываем файл для записи
+    if (out.is_open())
+    {
+        for (int i = 0; i < m; ++i){
+            std::vector<std::tuple<int,int,int>> edges = generateGraph(n, W);
+            out << pathFinder(path, edges) << " ";
+        }
+    }
     return 0;
 }
