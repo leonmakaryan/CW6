d2922aab00472506aec193be6693928ee7ddb4ed
Leon Makaryan <70325164+leonmakaryan@users.noreply.github.com>
2022-06-12 01:17:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,80 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
-#include <fstream>
+#include <utility>
 #include "mersenne_twister.h"
+#include <fstream>
 
-std::vector<int> path{1,1,2,1,1}; // paths we are looking for
+using namespace std;
+
 int random_seed = 1;
-
-void finderRec(std::vector<std::tuple<int,int,int>>& edges, int& p, int k, int& TotalNumber){
-    for (int i = 0; i < edges.size(); ++i){
-        if (std::get<2>(edges[i]) == path[k] && (p == std::get<0>(edges[i]) || p == std::get<1>(edges[i]))){
-            if (p == std::get<0>(edges[i]) && k + 1 < path.size())
-                finderRec(edges, std::get<1>(edges[i]), k + 1, TotalNumber);
-            else if (p == std::get<1>(edges[i]) && k + 1 < path.size())
-                finderRec(edges, std::get<0>(edges[i]), k + 1, TotalNumber);
-            else
-                TotalNumber++;
+void finderRec(const std::vector<int>& path, const std::vector<std::vector<std::pair<int, int>>>& adj, int& count, const int& verticeNumber, const int& previousVertice, int& len) {
+    for (int j = 0; j < adj.size(); ++j) {
+        if (adj[verticeNumber][j].second == path[len] && (verticeNumber != adj[verticeNumber][j].first) && (adj[verticeNumber][j].first != previousVertice)) {
+            len++;
+            if (len < path.size())
+                finderRec(path, adj, count, adj[verticeNumber][j].first, verticeNumber, len);
+            else if (len == path.size())
+                count++;
+            else if (len > path.size())
+                break;
         }
     }
 }
 
-int pathFinder(std::vector<int>& path, std::vector<std::tuple<int,int,int>>& edges){
-    int TotalNumber = 0;
-    for (auto& x: edges)
-        if (std::get<2>(x) == path[0]){
-            finderRec(edges, std::get<0>(x), 1, TotalNumber);
-            finderRec(edges, std::get<1>(x), 1, TotalNumber);
+int pathFinder(const std::vector<int>& path, const std::vector<std::vector<std::pair<int, int>>>& adj) {
+    int count = 0;
+    int len = 0; // current chain length
+    for (int i = 0; i <= adj.size()-1; ++i) 
+        for (int j = 0; j <= adj.size()-1; ++j)
+            if (adj[i][j].second == path[0]) {
+                len++;
+                finderRec(path, adj, count, adj[i][j].first, i, len);
+                len = 0;
             }
-    return TotalNumber;
+    return count;
 }
 
-std::vector<std::tuple<int,int,int>> generateGraph(int& n, std::vector<int>& W){
-    std::vector<std::tuple<int,int,int>> edges;
-    //int m = (n * (n - 1)) / 2; // number of edges in a complete graph
-    int j = 0; // number of the vertex next to the vertex i
-    int random;
-    int len = W.size(); // length of vector W
-    int sel_elem;
-    for (int i = 1; i <= n; ++i){
+std::vector<std::vector<std::pair<int, int>>> generateGraph(const int& size, const vector<int>& labels) {
+    std::vector<std::vector<std::pair<int, int>>> adj(size+1);
+    int j = 0; // number of the vertex next to the vertex i.
+    int len = labels.size() - 1; // length - 1 of vector labels 
+    int wt;
+    for (int i = 0; i <= size; ++i) {
         j = i + 1;
-        while (j <= n){
-            CRandomMersenne randgen_three(random_seed);
+        while (j <= size) {
+            CRandomMersenne randgen_three(random_seed+43);
             random_seed++;
-            sel_elem = W[randgen_three.IRandom(0, len)];
-            edges.push_back({i,j,sel_elem});
+            wt = labels[randgen_three.IRandom(0, len)];
+            adj[i].push_back(std::make_pair(j, wt));
+            adj[j].push_back(std::make_pair(i, wt));
             j++;
         }
     }
-    return edges;
+    return adj;
 }
 
-int main(){
-    std::cout << "Hello, World!" << "\n";
-    int n; // number of vertices
-    std::cout << "Input the number of vertices n: ";
-    std::cin >> n;
-    int m; // number of tests in Monte-Carlo
-    std::cout << "Input the number of tests m: ";
-    std::cin >> m;
-    std::vector<int> W{1,2,3,4,5,6}; // set of labels
-    //std::cout << "Input the desired set of labels W ";
-    //std::vector<int> path{1,2,3}; // paths we are looking for
-    //for (auto& x: edges)
-      //cj  std::cout << std::get<2>(x) << " ";
-    
-    std::cout << "\n";
-    //std::cout << "Total number of paths = " <<  pathFinder(path, edges) << "\n";
-    
+void printGraph(const std::vector<std::vector<std::pair<int, int>>>& adj, const int& size){
+    for (int i = 0; i <= size; ++i) 
+        for (const auto u : adj[i]) 
+            std::cout << i << " " << u.first << " " << u.second << "\n";  
+}
+
+int main()
+{
+	std::cout << "Hello, World!\n";
+    int size = 20;
+    int m = 500;
+    std::vector<int> labels{1,2}; // set of labels
+    std::vector<int> path{1,1,1,1}; // paths we are looking for
     std::ofstream out;// поток для записи
     out.open("MonteCarlo.txt"); // окрываем файл для записи
     if (out.is_open())
-    {
-        for (int i = 0; i < m; ++i){
-            std::vector<std::tuple<int,int,int>> edges = generateGraph(n, W);
-            out << pathFinder(path, edges) << " ";
+        for (int i = 0; i < m; ++i) {
+            std::vector<std::vector<std::pair<int, int>>> adj = generateGraph(size, labels);
+            out << pathFinder(path, adj) << " ";
         }
-    }
+    cout << "done!" << endl;
     return 0;
 }
